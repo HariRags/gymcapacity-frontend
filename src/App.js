88b3020172849a1b4e78f2f99cf7c4dd483cc181
Manish <msn07082005@gmail.com>
2024-03-08 23:29:14 +0530

    Created Separate files for List Page and removed them from App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,7 @@
-import "./App.css";
+
 import { useState } from "react";
-import Form from "./Form";
-import Popup from "./Popup";
 import Details from "./Details";
-
-function Page2() {
-  const Student = {
-    id: "",
-    name: "",
-  };
-  const [students, setStudents] = useState([]);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  function addStudent() {
-    const newStudent = { ...Student, id: generateUniqueId() };
-    setStudents([...students, newStudent]);
-  }
-  function removeStudent(studentID) {
-    setStudents(students.filter((student) => student.id !== studentID));
-  }
-  // since i didn't have acess to id yet i made a random id generator
-  function generateUniqueId() {
-    return crypto.randomUUID();
-  }
-  return (
-
-    <div className="container">
-      <section className="main-content">
-        <header className="header">
-          {students.length} people are on the grind!
-        </header>
-        <article className="list">
-          {students.map((student) => (
-            <div key={student.id} id="item">
-              <button
-                onClick={() =>{ setIsPopupOpen(true); removeStudent(student.id);}}
-                id="removeButton"
-              >
-                {" "}
-                -{" "}
-              </button>
-              <div id="name">
-                <div className="text">Manish</div>
-              </div>
-            </div>
-          ))}
-          <button onClick={addStudent} id="addButton">
-            Enter
-          </button>
-        </article>
-      </section>
-      <aside className="feedback">{<Form/>}</aside>
-      <Popup isOpen={isPopupOpen} onClose={()=>setIsPopupOpen(false)}/>
-    </div>
-    
-  );
-}
-
+import Page2 from "./List";
 
 export default Page2;
+
