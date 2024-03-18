8bc5c8797a57c22dc02e3804e0781f7f9274f62f
Manish <msn07082005@gmail.com>
2024-03-18 10:39:56 +0530

    Changed CSS for Form for Laptop Only

--- src/Form.js
+++ src/Form.js
@@ -4,26 +4,23 @@ const Form = () => {
         <h2>Feedback Form</h2>
             <form action="#" method="post">
             <div class="form-group">
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="form-name" name="name" required/>
+                <input type="text" id="form-name" name="name" required placeholder="Name"/>
             </div>
             <div class="form-group">
-                <label htmlFor="roll-number">Roll Number:</label>
-                <input type="text" id="roll-number" name="roll-number" required/>
+                <input type="text" id="roll-number" name="roll-number" required placeholder="Roll No."/>
             </div>
             <div class="form-group">
-                <label htmlFor="form-type">Form Type:</label>
+                
                 <select id="form-type" name="form-type" required>
-                <option value="">Select Form Type</option>
-                <option value="Complaint">Complaint</option>
-                <option value="Suggestion">Suggestion</option>
-                <option value="Appreciation">Appreciation</option>
-                <option value="Other">Other</option>
+                    <option value="" disabled selected>Form Type</option>
+                    <option value="Complaint">Complaint</option>
+                    <option value="Suggestion">Suggestion</option>
+                    <option value="Appreciation">Appreciation</option>
+                    <option value="Other">Other</option>
                 </select>
             </div>
-            <div class="form-group">
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" rows="4" required></textarea>
+            <div class="form-group-message">
+                <textarea placeholder="Message" id="message" name="message" rows="4" required></textarea>
             </div>
             <button type="submit">Submit</button>
             </form>
