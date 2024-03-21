11b2e7a39f018542d7288a02d26b4022518b4dc0
Priyam raj <rajpriyam27@gmail.com>
2024-03-21 20:00:30 +0530

    popup hide outside click

--- src/Lastpage.js
+++ src/Lastpage.js
@@ -1,89 +1,65 @@
-import "./Popup.css";
-import { useRef, useState, useEffect } from 'react';
-import { UserRound } from 'lucide-react';
-import { useHistory } from "react-router";
-import Lastpage from "./Lastpage";
+// src/lastpagePage.js
+import React, { useState,useEffect,useRef } from 'react';
+import './Lastpage.css'
+import Popup from './Popup'; // Import the Popup component
+import { useHistory } from 'react-router-dom';
 
-const Popup = ({ isOpen, onClose, student }) => {
-  const history = useHistory();
-  const PopupRef = useRef();
-  const [isInvalidPassword, setIsInvalidPassword] = useState(false);
-  const [isPending,setIsPending]=useState(false);
-  const [isCorrectPassword, setIsCorrectPassword] = useState(false);
-  const [showLastPage, setShowLastPage] = useState(false); // Track if Lastpage should be shown
+  
+  function Lastpage({onClose}) {
+    const LastpageRef = useRef();
+    const [showPopup, setShowPopup] = useState(false);
+    const history = useHistory();
 
-  useEffect(() => {
-    let timer;
-    if (showLastPage) {
-      timer = setTimeout(() => {
-        onClose(); // Close the popup after 5 seconds
-        history.push("/home"); // Redirect to list page after 2 seconds
-      }, 2000);
-    }
-
-    return () => clearTimeout(timer);
-  }, [showLastPage, onClose, history]);
-
-  const closePopup = (e) => {
-    if (PopupRef.current && !PopupRef.current.contains(e.target)) {
-      setIsInvalidPassword(false);
-      onClose();
-    }
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setIsPending(true);
-
-    const rollNumber = PopupRef.current.querySelector('#confirm-exit').value;
-    try {
-      const response = await fetch(`http://127.0.0.1:8000/gym/delete/${student.id}/`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          password: rollNumber,
-        })
-      });
-      if (response.ok) {
-        console.log('Student deleted Successfully!!');
-        setIsPending(false);
-        setIsCorrectPassword(true);
-        setShowLastPage(true); // Set to true when password is correct to show Lastpage
-        
-      } else {
-        setIsInvalidPassword(true);
-        setIsPending(false);
-        console.error('Failed to delete Student', response.statusText);
+    const closeLastpage = (e) => {
+      if (!LastpageRef.current.contains(e.target)) {
+        onClose();
       }
-    } catch (error) {
-      console.error('Error Deleting Student: ', error);
     }
-  }
+  
 
-  return isOpen ? (
-    <div className="popup-overlay" onClick={closePopup}>
-      <div className="popup-heading">
-        <p>Please confirm your Roll No.</p>
-      </div>
-      <div className="popup-form" ref={PopupRef}>
-        <form onSubmit={handleSubmit}>
-        {isInvalidPassword && <p className="wrong-rollno">Incorrect Roll No. Please try again.</p>}
-          <div className="roll-exit">
-            <UserRound className="User" />
-            <input type="text" placeholder="Roll No." id="confirm-exit" name="exit-roll" required />
-          </div>
-          <div >
-            {!isPending && <button className="exit-button" type="submit">Exit</button>}
-            {isPending && <button className="exit-button" type="submit">Verifying...</button>}
-          </div>
-        </form>
-      </div>
-      {isCorrectPassword && showLastPage && <Lastpage student={student} />} {/* Render Lastpage if password is correct and showLastPage is true */}
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPopup(false);
+            history.push('/home'); // Go back to the list page after 2 seconds
+          }, 2000);
+    
+        // Cleanup the timer on component unmount
+        return () => clearTimeout(timer);
+    }, [history]);
+    
+    const [deletedStudent, setDeletedStudent] = useState({name:'', id:''});
+    useEffect(() => {
+      async function fetchStudents() {
+        try {
+          const response = await fetch('http://127.0.0.1:8000/gym/last-deleted-user/');
+          if (!response.ok)
+            throw new Error('Failed to fetch students');
+          const data = await response.json();
+          setDeletedStudent(data);
+        } catch (error) {
+          console.error('Error fetching students', error);
+        }
+      }
+      fetchStudents();
+    }, []);
+  return (
+    <div ref={LastpageRef} onClick={closeLastpage} className="lastpagecontainer">
+    <div className="lastpg">
+      
+      <p>You have exited the gym.<br></br>
+      Rest well <p5>{deletedStudent.username} </p5>and<br></br> come back soon!</p>
+      
+      
+    </div> 
+    <Popup isOpen={showPopup} onClose={() => setShowPopup(false)}  />
     </div>
-  ) : null;
+
+    
+      
+    
+  );
 };
 
-export default Popup;
+export default Lastpage;
+
+
